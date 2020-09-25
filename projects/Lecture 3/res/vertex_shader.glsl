f670f084a67194e1e55e35ec171ab01714cbb240
s <s>
2020-09-25 14:48:11 -0400

    COMMITMENT

---
+++ projects/Lecture 3/res/vertex_shader.glsl
@@ -0,0 +1,14 @@
+#version 410
+
+layout (location = 0) in vec3 inPosition;
+layout (location = 1) in vec3 inColor;
+
+layout (location = 1) out vec3 outColor;
+
+void main()
+{
+	// vertex position are in clip space
+	gl_Position = vec4(inPosition, 1.0);
+
+	outColor = inColor;
+}
